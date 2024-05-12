f213acfc84a28fa8e56798b2e6472f45cc2b296c
Amal1902 <amal.elmoussaoui@student.ap.be>
2024-05-12 17:14:49 +0200

    footer en locatie gewijzigd

--- js/mapscript.js
+++ js/mapscript.js
@@ -2,7 +2,7 @@
 Gebruik hiervoor de documentatie op https://leafletjs.com/ 
 */
 
-let map = L.map('apMap').setView([51.23009, 4.41616], 17);
+let map = L.map('apMap').setView([51.672677135289625, -1.2628907159824847], 17);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -11,14 +11,14 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
 /* bepaal de rechthoek rondom het gebouw van AP */
-let bounds = [[51.23041, 4.4155], [51.22991, 4.41675]];
+let bounds = [[51.6730314339476, -1.2630087331878088],[51.67302311710951, -1.2620350912829619],[51.67229122937874, -1.26302214423333],[51.67230619992809, -1.26176955258164]];
 
 /* kleur de rechthoek in met de rode AP-kleur */
 L.rectangle(bounds, {color: "#9F750F", weight: 1}).addTo(map);
 
 /* plaats een marker met als tekst "AP-Hogeschool" en eronder "Ellermanstraat 33" */
-let marker = L.marker([51.23009, 4.41616]).addTo(map);
-marker.bindPopup("<b>Hoofdkantoor Safezone</b><br>Ellermanstraat 33").openPopup();
+let marker = L.marker([51.672677135289625, -1.2628907159824847]).addTo(map);
+marker.bindPopup("<b>Hoofdkantoor Safezone Corp.</b><br>Science Park, The Pentagon, Barton Ln, Abingdon OX14 3YP, Verenigd Koninkrijk").openPopup();
 
 
-L.marker([51.23009, 4.41616],).addTo(map);
+L.marker([51.672677135289625, -1.2628907159824847],).addTo(map);
